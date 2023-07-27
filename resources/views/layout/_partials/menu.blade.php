e2003cc2455d022ad45a9d742cd7e562b027fcce
ivan <ivandvelazquez99@gmail.com>
2023-07-27 18:15:13 -0300

    EN PROGRESO| Se agrega partial y layout de login

---
+++ resources/views/layout/_partials/menu.blade.php
@@ -0,0 +1,12 @@
+<header>
+    <nav style="padding-left: 10px; padding-right: 10px;" class="navbar bg-secondary navbar-expand-sm ">
+        <div class="container-fluid">
+            <ul class="navbar-nav me-auto order-0 ">
+                <li style="margin: 10px"><button style="padding:10px;" class="navbar-nav btn btn-success btn-lg"><a style="color: white;" href="{{route('garage.create')}}"">Crear cochera</a></button></li>
+                <li style="margin: 10px"><button style="padding:10px;" class="navbar-nav btn btn-success btn-lg"><a style="color: white;" href="{{route('car.create')}}"">Crear auto</a></button></li>
+                <li style="margin: 10px"><button style="padding:10px;" class="navbar-nav btn btn-primary btn-lg"><a style="color: white;" href="{{route('car.index')}}"">Ver autos</a></button></li>
+                <li style="margin: 10px"><button style="padding:10px;" class="navbar-nav  btn btn-primary btn-lg"><a style="color: white;" href="{{route('garage.index')}}"">Ver cocheras</a></button></li>
+            </ul>
+        </div>
+    </nav>
+</header>
